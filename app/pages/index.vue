50480d3b18172969a27d7d639498dbc9f15fc7ae
Linus Fischer <leicraft@leicraftmc.de>
2026-05-30 16:17:44 +0200

    added index.vue redirect

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+useSeoMeta({
+    title: "Redirecting... | LeiCraft_MC Hosting",
+    description: "Sie werden weitergeleitet...",
+});
+
+await navigateTo("https://hosting.leicraftmc.de", { external: true });
+
+</script>
+
+<template>
+    <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">Redirecting...</h1>
+        <p class="text-lg text-gray-600">Sie werden weitergeleitet...</p>
+    </div>
+</template>
